ddca5d94b4ae543c51ecb47888446f2381bca11a
Calvin <developerleebo@gmail.com>
2025-02-07 14:33:08 +0800

    feat: add socket url

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,6 +2,7 @@ import { env } from 'node:process';
 
 export default () => ({
   port: parseInt(env.PORT || '3000', 10),
+  wssUrl: env.WSS_URL,
   token: env.TOKEN,
   aesKey: env.AES_KEY,
   password: env.PASSWORD,
